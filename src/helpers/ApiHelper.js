c8785fc837d5e6265b8fff00e54b9ae7b140c7ca
bmaupin <bmaupin@users.noreply.github.com>
2018-10-23 13:40:09 -0400

    Move top languages scores logic into its own class

--- src/helpers/ApiHelper.js
+++ src/helpers/ApiHelper.js
@@ -13,33 +13,33 @@ class ApiHelper {
   //   return chartData;
   // }
 
-  static async _getSeriesData(chartType, dates) {
-    let chartLanguages = await ApiHelper._getChartLanguages(chartType, dates);
+  // static async _getSeriesData(chartType, dates) {
+  //   let chartLanguages = await ApiHelper._getChartLanguages(chartType, dates);
 
-    return await ApiHelper._getScoresForChart(chartType, chartLanguages, dates);
-  }
+  //   return await ApiHelper._getScoresForChart(chartType, chartLanguages, dates);
+  // }
 
-  static async _getChartLanguages(chartType, dates) {
-    switch(chartType) {
-      case ApiHelper.CHART_TYPES.TOP_LANGUAGES:
-        return await ApiHelper._getTopLanguages();
-      case ApiHelper.CHART_TYPES.FASTEST_OVER_100:
-        return await ApiHelper._getFastestGrowingLanguages(dates, 100);
-      case ApiHelper.CHART_TYPES.FASTEST_OVER_1000:
-        return await ApiHelper._getFastestGrowingLanguages(dates, 1000);
-      default:
-        throw new Error(`Unknown chart type: ${chartType}`);
-    }
-  }
+  // static async _getChartLanguages(chartType, dates) {
+  //   switch(chartType) {
+  //     case ApiHelper.CHART_TYPES.TOP_LANGUAGES:
+  //       return await ApiHelper._getTopLanguages();
+  //     case ApiHelper.CHART_TYPES.FASTEST_OVER_100:
+  //       return await ApiHelper._getFastestGrowingLanguages(dates, 100);
+  //     case ApiHelper.CHART_TYPES.FASTEST_OVER_1000:
+  //       return await ApiHelper._getFastestGrowingLanguages(dates, 1000);
+  //     default:
+  //       throw new Error(`Unknown chart type: ${chartType}`);
+  //   }
+  // }
 
-  static async _getScoresForChart(chartType, languages, dates) {
-    let apiFilter = ApiHelper._buildSeriesApiFilter(languages, dates);
-    let scoresFromApi = await ApiHelper._callApi(apiFilter);
-    let calculatedSeriesData = ApiHelper._calculateDataForChart(chartType, scoresFromApi);
-    let formattedSeriesData = ApiHelper._formatDataForChart(languages, calculatedSeriesData);
+  // static async _getScoresForChart(chartType, languages, dates) {
+  //   let apiFilter = ApiHelper._buildSeriesApiFilter(languages, dates);
+  //   let scoresFromApi = await ApiHelper._callApi(apiFilter);
+  //   let calculatedSeriesData = ApiHelper._calculateDataForChart(chartType, scoresFromApi);
+  //   let formattedSeriesData = ApiHelper._formatDataForChart(languages, calculatedSeriesData);
 
-    return formattedSeriesData;
-  }
+  //   return formattedSeriesData;
+  // }
 
   static _buildSeriesApiFilter(languages, dates) {
     return {
@@ -59,63 +59,63 @@ class ApiHelper {
     };
   }
 
-  static _calculateDataForChart(chartType, scores) {
-    // intermediate format: calculatedData = {'languagename': [score, score], 'languagename': [score, score]}
+  // static _calculateDataForChart(chartType, scores) {
+  //   // intermediate format: calculatedData = {'languagename': [score, score], 'languagename': [score, score]}
 
-    let calculatedData = {};
+  //   let calculatedData = {};
 
-    for (let i = 0; i < scores.length; i++) {
-      const languageName = scores[i].language.name;
-      const points = scores[i].points;
+  //   for (let i = 0; i < scores.length; i++) {
+  //     const languageName = scores[i].language.name;
+  //     const points = scores[i].points;
 
-      if (!calculatedData.hasOwnProperty(languageName)) {
-        calculatedData[languageName] = [];
-      }
-      calculatedData[languageName].push(points);
-    }
+  //     if (!calculatedData.hasOwnProperty(languageName)) {
+  //       calculatedData[languageName] = [];
+  //     }
+  //     calculatedData[languageName].push(points);
+  //   }
 
-    return calculatedData;
-  }
+  //   return calculatedData;
+  // }
 
-  static _formatDataForChart(languages, calculatedSeriesData) {
-    let formattedScores = [];
+  // static _formatDataForChart(languages, calculatedSeriesData) {
+  //   let formattedScores = [];
 
-    languages.forEach(languageName => {
-      let languageData = [];
+  //   languages.forEach(languageName => {
+  //     let languageData = [];
 
-      for (let i = 0; i < calculatedSeriesData[languageName].length; i++) {
-        languageData.push(
-          {
-            x: i,
-            y: calculatedSeriesData[languageName][i],
-          }
-        );
-      }
+  //     for (let i = 0; i < calculatedSeriesData[languageName].length; i++) {
+  //       languageData.push(
+  //         {
+  //           x: i,
+  //           y: calculatedSeriesData[languageName][i],
+  //         }
+  //       );
+  //     }
 
-      formattedScores.push(
-        {
-          title: languageName,
-          data: languageData,
-        }
-      );
-    });
+  //     formattedScores.push(
+  //       {
+  //         title: languageName,
+  //         data: languageData,
+  //       }
+  //     );
+  //   });
 
-    // for (let i = 0; i < scores.length; i++) {
-    //   const languageName = scores[i].language.name;
-    //   const points = scores[i].points;
+  //   // for (let i = 0; i < scores.length; i++) {
+  //   //   const languageName = scores[i].language.name;
+  //   //   const points = scores[i].points;
 
-    //   let languageData = formattedScores.find(languageData => languageData.title === languageName);
+  //   //   let languageData = formattedScores.find(languageData => languageData.title === languageName);
 
-    //   languageData.data.push(
-    //     {
-    //       x: languageData.data.length,
-    //       y: points,
-    //     }
-    //   )
-    // }
+  //   //   languageData.data.push(
+  //   //     {
+  //   //       x: languageData.data.length,
+  //   //       y: points,
+  //   //     }
+  //   //   )
+  //   // }
 
-    return formattedScores;
-  }
+  //   return formattedScores;
+  // }
 
   // TODO: this is a hot mess
   static async _getFastestGrowingLanguages(dates, minimumScore) {
@@ -123,6 +123,9 @@ class ApiHelper {
     let scores = await ApiHelper._getScoresForDates([lastDate, previousDate]);
     let scoresByLanguage = {};
     let languageNames = {};
+
+    console.log(`SCORES=${JSON.stringify(scores)}`);
+    // [{"date":"2018-05-01T00:00:00.000Z","points":112583,"id":19896,"languageId":8,"language":{"name":"Haskell","stackoverflowTag":null,"id":8}},{"date": ...},
 
     for (let i = 0; i < scores.length; i++) {
       const date = scores[i].date;
@@ -138,6 +141,9 @@ class ApiHelper {
       scoresByLanguage[languageId][date] = points;
     }
 
+    console.log(scoresByLanguage);
+    // {1: {2018-05-01T00:00:00.000Z: 33054, 2018-08-01T00:00:00.000Z: 33178}, 2: ...}
+
     let scoreDifferences = {};
 
     for (let langaugeId in scoresByLanguage) {
@@ -149,12 +155,23 @@ class ApiHelper {
       }
     }
 
+    console.log(scoreDifferences);
+    // 1:124, 2:54451
+
     let sortedScores = Object.keys(scoreDifferences).sort(function(a,b) {return scoreDifferences[b] - scoreDifferences[a];});
     let fastestGrowingLanguages = new Map();
 
     for (let i = 0; i < ApiHelper.NUMBER_OF_LANGUAGES; i++) {
       fastestGrowingLanguages.set(sortedScores[i], languageNames[sortedScores[i]]);
     }
+
+    console.log(sortedScores);
+    // 0:"10", 1:"9", 2:"15"
+
+    console.log(fastestGrowingLanguages);
+    // 0:{"10" => "JavaScript"}, 1:{"9" => "Java"}, 2:{"15" => "Python"}
+
+    // chartData=[{"title":"JavaScript","data":[{"x":0,"y":2466983},...,{"title":"Java","data":[{"x":0,"y":2199100},
 
     return fastestGrowingLanguages;
   }
@@ -176,6 +193,8 @@ class ApiHelper {
 
   static async _callApi(filter) {
     let apiUrl = encodeURI(`${API_BASE_URL}/api/scores?filter=${JSON.stringify(filter)}&access_token=${API_TOKEN}`);
+
+    console.log(apiUrl)
 
     let response = await fetch(apiUrl);
     return response.json();
@@ -220,11 +239,11 @@ class ApiHelper {
   }
 }
 
-ApiHelper.CHART_TYPES = {
-  TOP_LANGUAGES: 'toplanguages',
-  FASTEST_OVER_100: 'fastestover100',
-  FASTEST_OVER_1000: 'fastestover1000',
-};
+// ApiHelper.CHART_TYPES = {
+//   TOP_LANGUAGES: 'toplanguages',
+//   FASTEST_OVER_100: 'fastestover100',
+//   FASTEST_OVER_1000: 'fastestover1000',
+// };
 ApiHelper.NUMBER_OF_LANGUAGES = 10;
 
 export default ApiHelper;
