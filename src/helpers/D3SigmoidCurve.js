3be0383b99acc57c17079f4c32fe9a9b0f65fe47
bmaupin <bmaupin@users.noreply.github.com>
2019-04-24 13:34:45 -0400

    Use sigmoid curves for chart
    This makes the chart much more pleasing to look at, à la https://www.kevinflerlage.com/2019/03/curvy-bump-chart-slope-chart-template.html

--- src/helpers/D3SigmoidCurve.js
+++ src/helpers/D3SigmoidCurve.js
@@ -1,8 +1,24 @@
-function Linear(context) {
-  this._context = context;
+function point(that, x, y) {
+  that._context.bezierCurveTo(
+    that._prevX + ((x - that._prevX) * that._compression),
+    that._prevY,
+    x - ((x - that._prevX) * that._compression),
+    y,
+    x,
+    y,
+  );
 }
 
-Linear.prototype = {
+// I'm not sure if "compression" is the proper mathematical term...
+// A compression between 0 (straight lines) and 1 will give the best results
+// The closer you approach 1, the cleaner the lines will look but the more difficult it will be to distinguish between
+// multiple lines
+function D3SigmoidCurve(context, compression) {
+  this._context = context;
+  this._compression = compression;
+}
+
+D3SigmoidCurve.prototype = {
   areaStart: function() {
     this._line = 0;
   },
@@ -21,11 +37,21 @@ Linear.prototype = {
     switch (this._point) {
       case 0: this._point = 1; this._line ? this._context.lineTo(x, y) : this._context.moveTo(x, y); break;
       case 1: this._point = 2; // proceed
-      default: this._context.lineTo(x, y); break;
+      default: point(this, x, y); break;
     }
-  }
+    this._prevX = x;
+    this._prevY = y;
+  },
 };
 
-export default function(context) {
-  return new Linear(context);
-}
+export default (function custom(compression) {
+  function cardinal(context) {
+    return new D3SigmoidCurve(context, compression);
+  }
+
+  cardinal.compression = function(compression) {
+    return custom(compression);
+  };
+
+  return cardinal;
+})(0.75);
