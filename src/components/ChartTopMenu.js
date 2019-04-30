dec52755327c3e5415677a8b3880aa76a6c470d2
bmaupin <bmaupin@users.noreply.github.com>
2019-04-30 13:56:05 -0400

    Remove 'top' tab for now

--- src/components/ChartTopMenu.js
+++ src/components/ChartTopMenu.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
 
 export default class ChartTopMenu extends Component {
-  state = { activeItem: 'top' }
+  state = { activeItem: 'fastest growing' }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -11,8 +11,9 @@ export default class ChartTopMenu extends Component {
 
     return (
       <Menu secondary>
-        <Menu.Item name='top' active={activeItem === 'top'} onClick={this.handleItemClick} />
         <Menu.Item name='fastest growing' active={activeItem === 'fastest growing'} onClick={this.handleItemClick} />
+        {/* TODO */}
+        {/* <Menu.Item name='top' active={activeItem === 'top'} onClick={this.handleItemClick} /> */}
       </Menu>
     )
   }
