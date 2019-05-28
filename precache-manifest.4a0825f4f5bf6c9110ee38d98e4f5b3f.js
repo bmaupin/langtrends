1234f8850dbcafb3c1b374a418f2dae2efb95966
bmaupin <bmaupin@users.noreply.github.com>
2019-05-28 13:46:27 -0400

    Updates

---
+++ precache-manifest.4a0825f4f5bf6c9110ee38d98e4f5b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ea3f452b2408e1b473458d1efe6e5b",
+    "url": "/langtrends/index.html"
+  },
+  {
+    "revision": "574f27ffb1328d490c0d",
+    "url": "/langtrends/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "555ff93c8c41ba84eb0c",
+    "url": "/langtrends/static/css/main.b8ab7400.chunk.css"
+  },
+  {
+    "revision": "574f27ffb1328d490c0d",
+    "url": "/langtrends/static/js/2.a6155e98.chunk.js"
+  },
+  {
+    "revision": "555ff93c8c41ba84eb0c",
+    "url": "/langtrends/static/js/main.e573c379.chunk.js"
+  },
+  {
+    "revision": "0c90df2d624a85e1e4df",
+    "url": "/langtrends/static/js/runtime~main.27a77da2.js"
+  }
+]);
