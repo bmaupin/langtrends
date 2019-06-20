59a4b389356c8ea0b5bd0f1293fcffb189206d20
bmaupin <bmaupin@users.noreply.github.com>
2019-06-20 13:52:06 -0400

    First hacky attempt at converting top chart to bump chart

--- src/helpers/TopLanguagesChart.js
+++ src/helpers/TopLanguagesChart.js
@@ -18,9 +18,26 @@ export default class TopLanguagesChart {
   async getSeries() {
     const languages = await this._getLanguages();
     const scoresForSeries = await ApiHelper.getScoresForSeries(languages, await this.getDates());
-    let formattedSeriesData = this._formatDataForChart(languages, scoresForSeries);
+    let formattedSeriesData = await this._formatDataForChart(languages, scoresForSeries);
 
     return formattedSeriesData;
+  }
+
+  // Organize scores by date so we can access each one directly
+  static _organizeScoresByDate(scores) {
+    let scoresByDate = {};
+    for (let i = 0; i < scores.length; i++) {
+      const date = scores[i].date;
+      const languageName = scores[i].language.name;
+      const points = scores[i].points;
+
+      if (!scoresByDate.hasOwnProperty(date)) {
+        scoresByDate[date] = {};
+      }
+      scoresByDate[date][languageName] = points;
+    }
+
+    return scoresByDate;
   }
 
   async _getLanguages() {
@@ -46,8 +63,18 @@ export default class TopLanguagesChart {
     return topLanguages;
   }
 
-  _formatDataForChart(languages, scores) {
+  async _formatDataForChart(languages, scores) {
     let formattedScores = [];
+
+    // TODO: hack
+    const dates = await this.getDates();
+    // scores = await TopLanguagesChart._getAllScores(dates);
+    const scoresByDate = TopLanguagesChart._organizeScoresByDate(scores);
+
+    console.log(dates)
+    console.log(scoresByDate)
+
+    console.log(languages)
 
     languages.forEach(languageName => {
       formattedScores.push(
@@ -64,10 +91,16 @@ export default class TopLanguagesChart {
 
       let languageData = formattedScores.find(languageData => languageData.title === languageName);
 
+      // let rank = Object.keys(scoresByDate[dates[languageData.data.length]]).indexOf(languageName) + 1;
+      let rank = Object.keys(scoresByDate[dates[languageData.data.length]]).sort((a, b) => {return scoresByDate[dates[languageData.data.length]][b] - scoresByDate[dates[languageData.data.length]][a]}).indexOf(languageName) + 1;
+
       languageData.data.push(
         {
           x: languageData.data.length,
-          y: points,
+          y: rank,
+          hintTitle: languageName,
+          // Add the actual score as a separate property so it can be used for hints on mouseover
+          hintValue: points,
         }
       );
     }
