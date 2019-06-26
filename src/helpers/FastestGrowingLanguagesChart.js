fd41240d58c2e8c214b9bcba11171f843955df85
bmaupin <bmaupin@users.noreply.github.com>
2019-06-26 13:42:45 -0400

    Refactor nearly all chart logic into LanguagesChart
    Technically this means that most growth and top languages will get additional filtering, for example scores will be filtered
    below minimumScore, but this is effectively a noop and it reduces a bunch of duplication and should make managing charts
    easier overall

--- src/helpers/FastestGrowingLanguagesChart.js
+++ src/helpers/FastestGrowingLanguagesChart.js
@@ -1,73 +1,8 @@
-import ApiHelper from './ApiHelper';
 import LanguagesChart from './LanguagesChart';
-import settings from '../settings.json';
 
 export default class FastestGrowingLanguagesChart extends LanguagesChart {
-  async getDates() {
-    // We need one extra date internally for calculations, so to avoid extra API calls just drop the extra date
-    return (await this._getDatesForCalculations()).slice(1);
-  }
-
-  async _getDatesForCalculations() {
-    if (typeof this._dates === 'undefined') {
-      let dates = await ApiHelper.buildDates(this._interval, settings.numberOfDates + 1);
-      // From this point on we only need the date as a string
-      this._dates = dates.map(date => date.toISOString());
-    }
-
-    return this._dates;
-  }
-
-  async getSeries() {
-    const datesForCalculations = await this._getDatesForCalculations();
-    const scoresFromApi = await ApiHelper.getAllScores(datesForCalculations);
-    const scoresByDate = LanguagesChart._organizeScoresByDate(scoresFromApi);
-    const percentageChangesByDate = this._getPercentageChangesByDate(scoresByDate, datesForCalculations);
-    const datesForChart = await this.getDates();
-    const topPercentageChanges = await LanguagesChart._calculateTopScores(percentageChangesByDate, datesForChart);
-    const formattedSeriesData = await this._formatDataForChart(topPercentageChanges, datesForChart);
-
-    return formattedSeriesData;
-  }
-
-  _getPercentageChangesByDate(scoresByDate, datesForCalculations) {
-    let percentageChangesByDate = {};
-
-    // Start from 1 because the previous date is just used for calculating the percentage change
-    for (let i = 1; i < datesForCalculations.length; i++) {
-      let date = datesForCalculations[i];
-      let previousDate = datesForCalculations[i - 1];
-      percentageChangesByDate[date] = {};
-
-      for (let languageName in scoresByDate[date]) {
-        // TODO: Filter by scores where the most recent score is above the minimum??
-        // if (scoresByDate[datesForCalculations[datesForCalculations.length - 1]][languageName] > settings.minimumScore) {
-        if (scoresByDate[date][languageName] > settings.minimumScore) {
-          let percentageChange = FastestGrowingLanguagesChart._calculatePercentageChange(
-            scoresByDate[previousDate][languageName],
-            scoresByDate[date][languageName]
-          );
-
-          // percentageChange could be NaN or Infinity, but react-vis can only handle numbers or null
-          percentageChange = FastestGrowingLanguagesChart._convertNonFiniteToNull(percentageChange);
-
-          percentageChangesByDate[date][languageName] = percentageChange;
-        }
-      }
-    }
-
-    return percentageChangesByDate;
-  }
-
-  static _calculatePercentageChange(oldValue, newValue) {
+  _calculateCustomScore(oldValue, newValue) {
     return newValue / oldValue * 100;
-  }
-
-  static _convertNonFiniteToNull(number) {
-    if (!Number.isFinite(number)) {
-      number = null;
-    }
-    return number;
   }
 
   _formatHintValue(hintValue) {
--- src/helpers/TopLanguagesChart.js
+++ src/helpers/TopLanguagesChart.js
@@ -1,24 +1,11 @@
-import ApiHelper from './ApiHelper';
 import LanguagesChart from './LanguagesChart';
 
 export default class TopLanguagesChart extends LanguagesChart {
-  async getDates() {
-    if (typeof this._dates === 'undefined') {
-      let dates = await ApiHelper.buildDates(this._interval);
-      // From this point on we only need the date as a string
-      this._dates = dates.map(date => date.toISOString());
-    }
-
-    return this._dates;
+  _calculateCustomScore(_oldValue, newValue) {
+    return newValue;
   }
 
-  async getSeries() {
-    const dates = await this.getDates();
-    const scoresFromApi = await ApiHelper.getAllScores(dates);
-    const scoresByDate = LanguagesChart._organizeScoresByDate(scoresFromApi);
-    const topScores = await LanguagesChart._calculateTopScores(scoresByDate, dates);
-    const formattedSeriesData = await this._formatDataForChart(topScores, dates);
-
-    return formattedSeriesData;
+  _formatHintValue(hintValue) {
+    return hintValue;
   }
 }
