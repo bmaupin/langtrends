0abd28c9e01f3f3ec6ba9a6346b9eb653e05cadb
bmaupin <bmaupin@users.noreply.github.com>
2019-06-26 13:42:45 -0400

    Refactor nearly all chart logic into LanguagesChart
    Technically this means that most growth and top languages will get additional filtering, for example scores will be filtered
    below minimumScore, but this is effectively a noop and it reduces a bunch of duplication and should make managing charts
    easier overall

--- src/helpers/LanguagesChart.js
+++ src/helpers/LanguagesChart.js
@@ -1,8 +1,36 @@
+import ApiHelper from './ApiHelper';
 import settings from '../settings.json';
 
 export default class LanguagesChart {
   constructor(interval) {
     this._interval = interval;
+  }
+
+  async getDates() {
+    // We need one extra date internally for calculations, so to avoid extra API calls just drop the extra date
+    return (await this._getDatesForCalculations()).slice(1);
+  }
+
+  async _getDatesForCalculations() {
+    if (typeof this._dates === 'undefined') {
+      let dates = await ApiHelper.buildDates(this._interval, settings.numberOfDates + 1);
+      // From this point on we only need the date as a string
+      this._dates = dates.map(date => date.toISOString());
+    }
+
+    return this._dates;
+  }
+
+  async getSeries() {
+    const datesForCalculations = await this._getDatesForCalculations();
+    const scoresFromApi = await ApiHelper.getAllScores(datesForCalculations);
+    const scoresByDate = LanguagesChart._organizeScoresByDate(scoresFromApi);
+    const customScoresByDate = this._getCustomScoresByDate(scoresByDate, datesForCalculations);
+    const datesForChart = await this.getDates();
+    const topCustomScores = await LanguagesChart._calculateTopScores(customScoresByDate, datesForChart);
+    const formattedSeriesData = await this._formatDataForChart(topCustomScores, datesForChart);
+
+    return formattedSeriesData;
   }
 
   // Organize scores by date so we can access each one directly
@@ -20,6 +48,43 @@ export default class LanguagesChart {
     }
 
     return scoresByDate;
+  }
+
+  // Convert raw scores into custom scores (percentage change, score difference, etc)
+  _getCustomScoresByDate(scoresByDate, datesForCalculations) {
+    let customScoresByDate = {};
+
+    // Start from 1 because the previous date is just used for calculating the custom score
+    for (let i = 1; i < datesForCalculations.length; i++) {
+      let date = datesForCalculations[i];
+      let previousDate = datesForCalculations[i - 1];
+      customScoresByDate[date] = {};
+
+      for (let languageName in scoresByDate[date]) {
+        // TODO: Filter by scores where the most recent score is above the minimum??
+        // if (scoresByDate[datesForCalculations[datesForCalculations.length - 1]][languageName] > settings.minimumScore) {
+        if (scoresByDate[date][languageName] > settings.minimumScore) {
+          let customScore = this._calculateCustomScore(
+            scoresByDate[previousDate][languageName],
+            scoresByDate[date][languageName]
+          );
+
+          // percentage change could be NaN or Infinity, but react-vis can only handle numbers or null
+          customScore = LanguagesChart._convertNonFiniteToNull(customScore);
+
+          customScoresByDate[date][languageName] = customScore;
+        }
+      }
+    }
+
+    return customScoresByDate;
+  }
+
+  static _convertNonFiniteToNull(number) {
+    if (!Number.isFinite(number)) {
+      number = null;
+    }
+    return number;
   }
 
   static async _calculateTopScores(scoresByDate, dates) {
@@ -77,7 +142,7 @@ export default class LanguagesChart {
             y: rank,
             // TODO: don't add hintTitle and hintValue if score is null
             hintTitle: languageName,
-            // Add the actual score as a separate property so it can be used for hints on mouseover
+            // Add the custom score as a separate property so it can be used for hints on mouseover
             hintValue: this._formatHintValue(score),
           }
         );
@@ -100,9 +165,5 @@ export default class LanguagesChart {
     }
 
     return allTopLanguages;
-  }
-
-  _formatHintValue(hintValue) {
-    return hintValue;
   }
 }
