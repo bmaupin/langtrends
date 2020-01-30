27083cd43f868c7dbc8f21ade007abf490402621
bmaupin <bmaupin@users.noreply.github.com>
2020-01-30 14:37:40 -0500

    Rename ChartData > ChartFactory
    and setChartData > loadChartData for clarity

--- src/components/ChartTopMenu.js
+++ src/components/ChartTopMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
-import ChartData from '../helpers/ChartData';
+import ChartFactory from '../helpers/ChartFactory';
 
 export default function ChartTopMenu(props) {
   const chartType = props.chartType;
@@ -9,18 +9,18 @@ export default function ChartTopMenu(props) {
   return (
     <Menu secondary>
       <Menu.Item
-        name={ChartData.CHART_TYPES.FASTEST_GROWTH}
-        active={chartType === ChartData.CHART_TYPES.FASTEST_GROWTH}
+        name={ChartFactory.CHART_TYPES.FASTEST_GROWTH}
+        active={chartType === ChartFactory.CHART_TYPES.FASTEST_GROWTH}
         onClick={props.handleItemClick}
       >Fastest growth</Menu.Item>
       <Menu.Item
-        name={ChartData.CHART_TYPES.MOST_GROWTH}
-        active={chartType === ChartData.CHART_TYPES.MOST_GROWTH}
+        name={ChartFactory.CHART_TYPES.MOST_GROWTH}
+        active={chartType === ChartFactory.CHART_TYPES.MOST_GROWTH}
         onClick={props.handleItemClick}
       >Most growth</Menu.Item>
       <Menu.Item
-        name={ChartData.CHART_TYPES.TOP_LANGUAGES}
-        active={chartType === ChartData.CHART_TYPES.TOP_LANGUAGES}
+        name={ChartFactory.CHART_TYPES.TOP_LANGUAGES}
+        active={chartType === ChartFactory.CHART_TYPES.TOP_LANGUAGES}
         onClick={props.handleItemClick}
       >Top</Menu.Item>
     </Menu>
--- src/helpers/ChartFactory.js
+++ src/helpers/ChartFactory.js
@@ -2,17 +2,17 @@ import TopLanguagesChart from './TopLanguagesChart';
 import FastestGrowingLanguagesChart from './FastestGrowingLanguagesChart';
 import MostGrowthLanguages from './MostGrowthLanguages';
 
-class ChartData {
+class ChartFactory {
   static async fromType(chartType, interval) {
     let chart;
     switch(chartType) {
-      case ChartData.CHART_TYPES.FASTEST_GROWTH:
+      case ChartFactory.CHART_TYPES.FASTEST_GROWTH:
         chart = new FastestGrowingLanguagesChart(interval);
         break;
-      case ChartData.CHART_TYPES.MOST_GROWTH:
+      case ChartFactory.CHART_TYPES.MOST_GROWTH:
         chart = new MostGrowthLanguages(interval);
         break;
-      case ChartData.CHART_TYPES.TOP_LANGUAGES:
+      case ChartFactory.CHART_TYPES.TOP_LANGUAGES:
         chart = new TopLanguagesChart(interval);
         break;
       default:
@@ -23,10 +23,10 @@ class ChartData {
   }
 }
 
-ChartData.CHART_TYPES = {
+ChartFactory.CHART_TYPES = {
   FASTEST_GROWTH: 'fastestgrowth',
   MOST_GROWTH: 'mostgrowth',
   TOP_LANGUAGES: 'toplanguages',
 };
 
-export default ChartData;
+export default ChartFactory;
