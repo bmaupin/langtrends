effec3f8621f6a88607fb6934e40c1badacc077e
bmaupin <bmaupin@users.noreply.github.com>
2020-01-31 13:28:04 -0500

    Add a minimal explanation as to how the data is calculated
    and what the different chart types mean
    
    Note that some of the formatting changes are a result of changes made by Prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import { Container, Icon, Menu } from 'semantic-ui-react';
+import { Container, Icon, Menu, Popup } from 'semantic-ui-react';
 
 export default class Header extends Component {
   render() {
     return (
       <Menu attached borderless inverted>
         <Container>
-          <Menu.Item fitted='horizontally' header>
+          <Menu.Item fitted="horizontally" header>
             Programming language trends
           </Menu.Item>
 
-          <Menu.Menu position='right'>
-            <Menu.Item href='https://github.com/bmaupin/langtrends' icon>
-              <Icon name='github' size='big' />
+          <Menu.Menu position="right">
+            <Popup
+              on="click"
+              trigger={
+                <Menu.Item icon>
+                  <Icon name="help circle" size="big" />
+                </Menu.Item>
+              }
+            >
+              <Popup.Content>
+                <h3>How the data is calculated</h3>
+                <p>
+                  First, a base numerical value for a given language and date is
+                  calculated by adding the total number of GitHub repositories
+                  to the total number of Stack Overflow tags for that language
+                  up to that day.
+                </p>
+                <h4>Fastest growth</h4>
+                <p>
+                  Languages with the highest percentage change compared to the
+                  previous date. Note that scores under a certain threshold (
+                  <a href="https://github.com/bmaupin/langtrends/blob/master/src/settings.json#L2">
+                    currently 1000
+                  </a>
+                  ) are filtered out to reduce{' '}
+                  <a href="https://xkcd.com/1102/">dubiousness</a>.
+                </p>
+                <h4>Most growh</h4>
+                <p>
+                  Languages with the highest numerical change compared to the
+                  previous date.
+                </p>
+                <h4>Top</h4>
+                <p>
+                  Languages with the total highest value for a particular given
+                  date.
+                </p>
+              </Popup.Content>
+            </Popup>
+            <Menu.Item href="https://github.com/bmaupin/langtrends" icon>
+              <Icon name="github" size="big" />
             </Menu.Item>
           </Menu.Menu>
         </Container>
