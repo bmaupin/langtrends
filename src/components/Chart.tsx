426191bca9046ff5d5d975fdc7b45b07101f59b9
bmaupin <bmaupin@users.noreply.github.com>
2021-10-21 13:45:03 -0400

    feat: Remove loading spinner and state
    So far it doesn't seem to be needed any more :D

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -9,7 +9,6 @@ import {
   XAxis,
   YAxis,
 } from 'react-vis';
-import { Dimmer, Loader, Image } from 'semantic-ui-react';
 
 import ChartFactory from '../helpers/ChartFactory';
 import D3SigmoidCurve from '../helpers/D3SigmoidCurve';
@@ -23,25 +22,11 @@ export default class Chart extends Component {
     super(props);
 
     this.state = {
-      chartData: null,
+      chartData: [],
       dates: [],
       hintValue: null,
       hoveredSeriesIndex: null,
-      isLoading: true,
-      showloadingMessage: false,
     };
-
-    // TODO: do we still need this?
-    // Don't show the loading message right away because it can create a lot of visual noise if the loading spinner
-    // only briefly flashes
-    this.loadingMessageTimeout = setTimeout(
-      () => {
-        this.setState({ showloadingMessage: true });
-      },
-      // This should be just long enough so it only shows when the API isn't ready but doesn't show in any other
-      // situations, such as when changing chart types when the data isn't cached yet
-      4000
-    );
 
     this._onValueMouseOut = this._onValueMouseOut.bind(this);
     this._onValueMouseOver = this._onValueMouseOver.bind(this);
@@ -68,28 +53,18 @@ export default class Chart extends Component {
       this.props.intervalInMonths
     );
 
-    // TODO: do we still need this?
-    this.setState({
-      isLoading: true,
-    });
-
     const dates = await this._chart.getDates();
     const series = await this._chart.getSeries();
 
     const leftYAxisLabels = Chart._generateLeftYAxisLabels(series);
     const rightYAxisLabels = Chart._generateRightYAxisLabels(series);
 
-    // Prevent the loading message from showing after the data is loaded
-    clearTimeout(this.loadingMessageTimeout);
-
     // TODO: just one object for chart data?
     this.setState({
       chartData: series,
       dates: dates,
-      isLoading: false,
       leftYAxisLabels: leftYAxisLabels,
       rightYAxisLabels: rightYAxisLabels,
-      showloadingMessage: false,
     });
   }
 
@@ -150,91 +125,66 @@ export default class Chart extends Component {
     return date.slice(0, 7);
   }
 
-  _renderLoadingSpinner() {
-    return (
-      <Dimmer.Dimmable blurring dimmed>
-        <Dimmer active inverted>
-          <Loader size="massive">
-            {this.state.showloadingMessage && (
-              <span>
-                Please wait
-                <div style={{ fontSize: '0.6em', marginTop: '0.5em' }}>
-                  (The backend may take up to 30 seconds to start)
-                </div>
-              </span>
-            )}
-          </Loader>
-        </Dimmer>
-
-        <Image src="assets/images/chart-placeholder.png" />
-      </Dimmer.Dimmable>
-    );
-  }
-
   render() {
-    if (this.state.isLoading || !this.state.chartData) {
-      return this._renderLoadingSpinner();
-    } else {
-      const d3sigmoidcurve = D3SigmoidCurve.compression(0.5);
-      return (
-        <div className="chart-container">
-          <div className="chart-content">
-            <FlexibleWidthXYPlot
-              height={settings.numberOfLanguages * 49}
-              margin={{
-                left: 80,
-                right: 80,
-              }}
-              // Reverse the y scale since we're doing a bump chart
-              yDomain={[settings.numberOfLanguages, 1]}
-            >
-              <VerticalGridLines />
-              <HorizontalGridLines />
-              <XAxis
-                tickFormat={this._xAxisLabelFormatter}
-                tickTotal={this.state.dates.length}
+    const d3sigmoidcurve = D3SigmoidCurve.compression(0.5);
+    return (
+      <div className="chart-container">
+        <div className="chart-content">
+          <FlexibleWidthXYPlot
+            height={settings.numberOfLanguages * 49}
+            margin={{
+              left: 80,
+              right: 80,
+            }}
+            // Reverse the y scale since we're doing a bump chart
+            yDomain={[settings.numberOfLanguages, 1]}
+          >
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis
+              tickFormat={this._xAxisLabelFormatter}
+              tickTotal={this.state.dates.length}
+            />
+            <YAxis
+              orientation="left"
+              tickFormat={(v, i) => this.state.leftYAxisLabels[i]}
+            />
+            <YAxis
+              orientation="right"
+              tickFormat={(v, i) => this.state.rightYAxisLabels[i]}
+            />
+            {this.state.chartData.map((entry, i) => (
+              <LineMarkSeries
+                curve={d3sigmoidcurve}
+                getNull={(d) => d.y !== null}
+                key={entry.title}
+                color={GitHubColors.get(entry.title, true).color}
+                data={entry.data}
+                opacity={
+                  this.state.hoveredSeriesIndex === null ||
+                  this.state.hoveredSeriesIndex === i
+                    ? 1
+                    : 0.5
+                }
+                onValueMouseOut={this._onValueMouseOut}
+                onValueMouseOver={(datapoint) =>
+                  this._onValueMouseOver(datapoint, i)
+                }
+                strokeWidth={
+                  this.state.hoveredSeriesIndex !== null &&
+                  this.state.hoveredSeriesIndex === i
+                    ? 4
+                    : null
+                }
+                lineStyle={{ pointerEvents: 'none' }}
               />
-              <YAxis
-                orientation="left"
-                tickFormat={(v, i) => this.state.leftYAxisLabels[i]}
-              />
-              <YAxis
-                orientation="right"
-                tickFormat={(v, i) => this.state.rightYAxisLabels[i]}
-              />
-              {this.state.chartData.map((entry, i) => (
-                <LineMarkSeries
-                  curve={d3sigmoidcurve}
-                  getNull={(d) => d.y !== null}
-                  key={entry.title}
-                  color={GitHubColors.get(entry.title, true).color}
-                  data={entry.data}
-                  opacity={
-                    this.state.hoveredSeriesIndex === null ||
-                    this.state.hoveredSeriesIndex === i
-                      ? 1
-                      : 0.5
-                  }
-                  onValueMouseOut={this._onValueMouseOut}
-                  onValueMouseOver={(datapoint) =>
-                    this._onValueMouseOver(datapoint, i)
-                  }
-                  strokeWidth={
-                    this.state.hoveredSeriesIndex !== null &&
-                    this.state.hoveredSeriesIndex === i
-                      ? 4
-                      : null
-                  }
-                  lineStyle={{ pointerEvents: 'none' }}
-                />
-              ))}
-              {this.state.hintValue && (
-                <Hint format={this._formatHint} value={this.state.hintValue} />
-              )}
-            </FlexibleWidthXYPlot>
-          </div>
+            ))}
+            {this.state.hintValue && (
+              <Hint format={this._formatHint} value={this.state.hintValue} />
+            )}
+          </FlexibleWidthXYPlot>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
