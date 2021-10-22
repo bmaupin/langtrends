9f7f910daec25cebbf380b8a49461d37c6acb42a
bmaupin <bmaupin@users.noreply.github.com>
2021-10-22 12:47:39 -0400

    fix: Set visibility for abstract methods implemented by LanguagesChart children

--- src/helpers/FastestGrowingLanguagesChart.ts
+++ src/helpers/FastestGrowingLanguagesChart.ts
@@ -1,11 +1,11 @@
 import LanguagesChart from './LanguagesChart';
 
 export default class FastestGrowingLanguagesChart extends LanguagesChart {
-  calculateCustomScore(oldValue: number, newValue: number) {
+  protected calculateCustomScore(oldValue: number, newValue: number) {
     return (newValue / oldValue) * 100;
   }
 
-  formatHintValue(hintValue: number): string {
+  protected formatHintValue(hintValue: number): string {
     return `${Math.round(hintValue)}% growth`;
   }
 }
