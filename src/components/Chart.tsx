9680ce7bab0ee2b842e6e8526c848a624035362e
bmaupin <bmaupin@users.noreply.github.com>
2021-10-22 12:03:02 -0400

    feat: Migrate from class components to hooks
    ... (in particular, useState)

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,5 @@
 import GitHubColors from 'github-colors';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   FlexibleWidthXYPlot,
   Hint,
@@ -17,58 +17,41 @@ import settings from '../settings.json';
 import './Chart.css';
 import '../../node_modules/react-vis/dist/style.css';
 
-export default class Chart extends Component {
-  constructor(props) {
-    super(props);
+export default function Chart(props: {
+  chartType: string;
+  intervalInMonths: number;
+}) {
+  const [chartData, setChartData] = useState([]);
+  const [dates, setDates] = useState([]);
+  const [hintValue, setHintValue] = useState(null);
+  const [hoveredSeriesIndex, setHoveredSeriesIndex] = useState(null);
+  const [leftYAxisLabels, setLeftYAxisLabels] = useState([]);
+  const [rightYAxisLabels, setRightYAxisLabels] = useState([]);
 
-    this.state = {
-      chartData: [],
-      dates: [],
-      hintValue: null,
-      hoveredSeriesIndex: null,
+  useEffect(() => {
+    const loadChartData = async () => {
+      const chart = await ChartFactory.fromType(
+        props.chartType,
+        props.intervalInMonths
+      );
+
+      // TODO: just one object for chart data?
+      const dates = await chart.getDates();
+      const series = await chart.getSeries();
+
+      const leftYAxisLabels = generateLeftYAxisLabels(series);
+      const rightYAxisLabels = generateRightYAxisLabels(series);
+
+      setChartData(series);
+      setDates(dates);
+      setLeftYAxisLabels(leftYAxisLabels);
+      setRightYAxisLabels(rightYAxisLabels);
     };
 
-    this._onValueMouseOut = this._onValueMouseOut.bind(this);
-    this._onValueMouseOver = this._onValueMouseOver.bind(this);
-    this._xAxisLabelFormatter = this._xAxisLabelFormatter.bind(this);
-  }
+    loadChartData();
+  }, [props.chartType, props.intervalInMonths]);
 
-  async componentDidMount() {
-    await this.loadChartData();
-  }
-
-  async componentDidUpdate(prevProps) {
-    // These conditionals prevent some extra flashing and wonkiness caused by reloading the chart data too aggressively
-    if (
-      this.props.chartType !== prevProps.chartType ||
-      this.props.intervalInMonths !== prevProps.intervalInMonths
-    ) {
-      await this.loadChartData();
-    }
-  }
-
-  async loadChartData() {
-    this._chart = await ChartFactory.fromType(
-      this.props.chartType,
-      this.props.intervalInMonths
-    );
-
-    const dates = await this._chart.getDates();
-    const series = await this._chart.getSeries();
-
-    const leftYAxisLabels = Chart._generateLeftYAxisLabels(series);
-    const rightYAxisLabels = Chart._generateRightYAxisLabels(series);
-
-    // TODO: just one object for chart data?
-    this.setState({
-      chartData: series,
-      dates: dates,
-      leftYAxisLabels: leftYAxisLabels,
-      rightYAxisLabels: rightYAxisLabels,
-    });
-  }
-
-  static _generateLeftYAxisLabels(series) {
+  const generateLeftYAxisLabels = (series) => {
     return (
       series
         // Get just the data for the first date
@@ -78,10 +61,10 @@ export default class Chart extends Component {
         // Drop everything else (x value, y value) and return just a list of hint titles
         .map((languageData) => languageData && languageData.hintTitle)
     );
-  }
+  };
 
   // TODO: remove duplication here?
-  static _generateRightYAxisLabels(series) {
+  const generateRightYAxisLabels = (series) => {
     return (
       series
         // Get just the data for the last date
@@ -91,100 +74,86 @@ export default class Chart extends Component {
         // Drop everything else (x value, y value) and return just a list of hint titles
         .map((languageData) => languageData && languageData.hintTitle)
     );
-  }
+  };
 
-  _formatHint(value) {
+  const formatHint = (value) => {
     return [
       {
         title: value.hintTitle,
         value: value.hintValue,
       },
     ];
-  }
+  };
 
-  _onValueMouseOut() {
-    this.setState({
-      hintValue: null,
-      hoveredSeriesIndex: null,
-    });
-  }
+  const onValueMouseOut = () => {
+    setHintValue(null);
+    setHoveredSeriesIndex(null);
+  };
 
-  _onValueMouseOver(value, index) {
-    this.setState({
-      hintValue: value,
-      hoveredSeriesIndex: index,
-    });
-  }
+  const onValueMouseOver = (value, index) => {
+    setHintValue(value);
+    setHoveredSeriesIndex(index);
+  };
 
   // TODO: could we just format the dates ahead of time and get rid of this method?
-  _xAxisLabelFormatter(_value, index) {
-    return Chart._formatDateForLabel(this.state.dates[index]);
-  }
+  const xAxisLabelFormatter = (_value, index) => {
+    return formatDateForLabel(dates[index]);
+  };
 
-  static _formatDateForLabel(date) {
-    return date.slice(0, 7);
-  }
+  const formatDateForLabel = (date) => {
+    // TODO: date can be undefined?
+    if (date) {
+      return date.slice(0, 7);
+    }
+  };
 
-  render() {
-    const d3sigmoidcurve = D3SigmoidCurve.compression(0.5);
-    return (
-      <div className="chart-container">
-        <div className="chart-content">
-          <FlexibleWidthXYPlot
-            height={settings.numberOfLanguages * 49}
-            margin={{
-              left: 80,
-              right: 80,
-            }}
-            // Reverse the y scale since we're doing a bump chart
-            yDomain={[settings.numberOfLanguages, 1]}
-          >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis
-              tickFormat={this._xAxisLabelFormatter}
-              tickTotal={this.state.dates.length}
+  const d3sigmoidcurve = D3SigmoidCurve.compression(0.5);
+
+  return (
+    <div className="chart-container">
+      <div className="chart-content">
+        <FlexibleWidthXYPlot
+          height={settings.numberOfLanguages * 49}
+          margin={{
+            left: 80,
+            right: 80,
+          }}
+          // Reverse the y scale since we're doing a bump chart
+          yDomain={[settings.numberOfLanguages, 1]}
+        >
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis tickFormat={xAxisLabelFormatter} tickTotal={dates.length} />
+          <YAxis orientation="left" tickFormat={(v, i) => leftYAxisLabels[i]} />
+          <YAxis
+            orientation="right"
+            tickFormat={(v, i) => rightYAxisLabels[i]}
+          />
+          {chartData.map((entry, i) => (
+            <LineMarkSeries
+              curve={d3sigmoidcurve}
+              getNull={(d) => d.y !== null}
+              key={entry.title}
+              color={GitHubColors.get(entry.title, true).color}
+              data={entry.data}
+              opacity={
+                hoveredSeriesIndex === null || hoveredSeriesIndex === i
+                  ? 1
+                  : 0.5
+              }
+              onValueMouseOut={onValueMouseOut}
+              onValueMouseOver={(datapoint) => onValueMouseOver(datapoint, i)}
+              strokeWidth={
+                hoveredSeriesIndex !== null && hoveredSeriesIndex === i
+                  ? 4
+                  : null
+              }
+              lineStyle={{ pointerEvents: 'none' }}
             />
-            <YAxis
-              orientation="left"
-              tickFormat={(v, i) => this.state.leftYAxisLabels[i]}
-            />
-            <YAxis
-              orientation="right"
-              tickFormat={(v, i) => this.state.rightYAxisLabels[i]}
-            />
-            {this.state.chartData.map((entry, i) => (
-              <LineMarkSeries
-                curve={d3sigmoidcurve}
-                getNull={(d) => d.y !== null}
-                key={entry.title}
-                color={GitHubColors.get(entry.title, true).color}
-                data={entry.data}
-                opacity={
-                  this.state.hoveredSeriesIndex === null ||
-                  this.state.hoveredSeriesIndex === i
-                    ? 1
-                    : 0.5
-                }
-                onValueMouseOut={this._onValueMouseOut}
-                onValueMouseOver={(datapoint) =>
-                  this._onValueMouseOver(datapoint, i)
-                }
-                strokeWidth={
-                  this.state.hoveredSeriesIndex !== null &&
-                  this.state.hoveredSeriesIndex === i
-                    ? 4
-                    : null
-                }
-                lineStyle={{ pointerEvents: 'none' }}
-              />
-            ))}
-            {this.state.hintValue && (
-              <Hint format={this._formatHint} value={this.state.hintValue} />
-            )}
-          </FlexibleWidthXYPlot>
-        </div>
+          ))}
+          {hintValue && <Hint format={formatHint} value={hintValue} />}
+        </FlexibleWidthXYPlot>
       </div>
-    );
-  }
+    </div>
+  );
 }
