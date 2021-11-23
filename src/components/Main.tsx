a0cab140be6735c1dafd6241a9e7c484270b0d8b
bmaupin <bmaupin@users.noreply.github.com>
2021-11-23 13:31:26 -0500

    feat: Store chart type/interval directly in the search params
    Seems like it works pretty well after all, and much less code than trying to keep search params in sync with separate state variables. I think when I tried this the first time I must've been handling the defaults differently so that it would always set the search params in the URL even before any of the buttons were pressed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ButtonProps, Container, Grid, Item } from 'semantic-ui-react';
 
@@ -10,34 +10,17 @@ import { ChartType } from '../helpers/ChartFactory';
 import './Main.css';
 
 export default function Main() {
-  // Keep a separate state outside searchParams for chartType and intervalInMonths so, for example, if someone just
-  // loads the default page we can use the default values without having to modify the URL
-  const [chartType, setChartType] = useState(ChartType.MostGrowth as string);
-  const [intervalInMonths, setIntervalInMonths] = useState(3);
-  let [searchParams, setSearchParams] = useSearchParams();
+  // Store the chart type/interval directly in the search params so we don't have to maintain separate state for them
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    if (
-      searchParams.get('interval') &&
-      Number(searchParams.get('interval')) !== intervalInMonths
-    ) {
-      setIntervalInMonths(Number(searchParams.get('interval')!));
-    }
-
-    if (
-      searchParams.get('chart_type') &&
-      searchParams.get('chart_type') !== chartType
-    ) {
-      setChartType(searchParams.get('chart_type')!);
-    }
-  }, [chartType, intervalInMonths, searchParams]);
+  const defaultChartType = ChartType.MostGrowth;
+  const defaultInterval = 3;
 
   const handleChartTypeChanged = (
     _event: React.MouseEvent<HTMLElement>,
     { name }: ButtonProps
   ) => {
     if (name) {
-      setChartType(name);
       // Setting the search params this way allows us to set certain params without overriding the others
       searchParams.set('chart_type', name);
       setSearchParams(searchParams);
@@ -48,7 +31,6 @@ export default function Main() {
     _event: React.MouseEvent<HTMLElement>,
     { value }: ButtonProps
   ) => {
-    setIntervalInMonths(Number(value));
     searchParams.set('interval', value);
     setSearchParams(searchParams);
   };
@@ -60,15 +42,22 @@ export default function Main() {
           <Item.Content>
             <Grid centered className="button-group-grid">
               <TopButtonGroup
-                chartType={chartType}
+                chartType={searchParams.get('chart_type') ?? defaultChartType}
                 handleItemClick={handleChartTypeChanged}
               />
             </Grid>
-            <Chart chartType={chartType} intervalInMonths={intervalInMonths} />
+            <Chart
+              chartType={searchParams.get('chart_type') ?? defaultChartType}
+              intervalInMonths={Number(
+                searchParams.get('interval') || defaultInterval
+              )}
+            />
             <Grid centered className="button-group-grid">
               <BottomButtonGroup
                 handleItemClick={handleIntervalChanged}
-                intervalInMonths={intervalInMonths}
+                intervalInMonths={Number(
+                  searchParams.get('interval') || defaultInterval
+                )}
               />
             </Grid>
           </Item.Content>
