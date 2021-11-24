59fe764aa593bf4d87b4b6fe3ee7973ef3bba3d9
bmaupin <bmaupin@users.noreply.github.com>
2021-11-24 14:23:41 -0500

    refactor: Convert remaining class components to functions
    I guess I must've missed them when I converted the others...

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Main from './Main';
 import Header from './Header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
-
-export default App;
